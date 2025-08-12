1044af292e37d368ddce7e7ece7ed2f40f96d584
unknown <tneimerman@gmail.com>
2025-08-11 21:18:23 -0300

    Matrices

---
+++ index.py
@@ -0,0 +1,17 @@
+referenciaUsuarios = ["DNI", "Nombre", "Apellido", "Telefono", "Fecha Nacimiento"]
+Usuarios = []
+
+referenciaDestinos = ["ID", "Destino", "Descripcion"]
+Destinos = []
+
+referenciaVuelos = ["ID", "Empresa", "IDDestino" "FechaLlegada", "Escala"]
+Vuelos = []
+
+referenciaActividades = ["ID", "Tipo", "Descripcion", "Fecha"]
+Actividades = []
+
+referenciaHoteleria = ["ID", "Nombre", "Descripcion", "Ubicacion", "Rating"]
+Hoteleria = []
+
+referenciaPack = ["ID", "Nombre", "Precio", "IDDestino", "IDVuelo", "IDActividad", "IDHotel"]
+Pack = []
